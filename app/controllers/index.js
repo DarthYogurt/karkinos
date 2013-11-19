0b0afa24e22e3ccb148a607d43918040f079236a
Raymond Walintukan <raytochina@gmail.com>
2013-11-19 13:47:59 -0800

    empty

--- app/controllers/index.js
+++
@@ -1,9 +0,0 @@
-function doClick(e) {
-    alert($.label.text);
-}
-
-function clickImage(e) {
-	Titanium.UI.createAlertDialog({title:'Image View', message:'You clicked me!'}).show();
-}
-
-$.index.open();
