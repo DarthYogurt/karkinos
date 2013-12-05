fe012c9bf989b5171ce15506ec51d9360632b98b
Deric Walintukan <deric.walintukan@gmail.com>
2013-12-04 18:02:11 -0800

    changed all functions to private

--- src/com/karkinos/croupon/Deal.java
+++ src/com/karkinos/croupon/Deal.java
@@ -24,122 +24,122 @@ public class Deal {
     public Deal() {
     }
     
-    public int getId() {
+    private int getId() {
     	return this.id;
     }
-    public void setId(int i) {
+    private void setId(int i) {
     	this.id = i;
     }
     
-    public int getImage() {
+    private int getImage() {
     	return this.image;
     }
-    public void setImage(int i) {
+    private void setImage(int i) {
     	this.image = i;
     }
     
-    public String getTitle() {
+    private String getTitle() {
     	return this.title;
     }
-    public void setTitle(String s) {
+    private void setTitle(String s) {
     	this.title = s;
     }
     
-    public String getDescription() {
+    private String getDescription() {
     	return this.description;
     }
-    public void setDescription(String s) {
+    private void setDescription(String s) {
     	this.description = s;
     }
     
-    public int getCurrentSupporters() {
+    private int getCurrentSupporters() {
     	return this.currentSupporters;
     }
-    public void setCurrentSupporters(int i) {
+    private void setCurrentSupporters(int i) {
     	this.currentSupporters = i;
     }
     
-    public int getMaxSupporters() {
+    private int getMaxSupporters() {
     	return this.maxSupporters;
     }
-    public void setMaxSupporters(int i) {
+    private void setMaxSupporters(int i) {
     	this.maxSupporters = i;
     }
     
-    public float getRegularPrice() {
+    private float getRegularPrice() {
     	return this.regularPrice;
     }
-    public void setRegularPrice(float i) {
+    private void setRegularPrice(float i) {
     	this.regularPrice = i;
     }
     
-    public float getDiscountPrice() {
+    private float getDiscountPrice() {
     	return this.discountPrice;
     }
-    public void setDiscountPrice(float i) {
+    private void setDiscountPrice(float i) {
     	this.discountPrice = i;
     }
  
-    public float getMsrp() {
+    private float getMsrp() {
     	return this.msrp;
     }
-    public void setMsrp(float f) {
+    private void setMsrp(float f) {
     	this.msrp = f;
     }
     
-    public float getLowestMarketPrice() {
+    private float getLowestMarketPrice() {
     	return this.lowestMarketPrice;
     }
-    public void setLowestMarketPrice(float f) {
+    private void setLowestMarketPrice(float f) {
     	this.lowestMarketPrice = f;
     }
     
-    public int getRank() {
+    private int getRank() {
     	return this.rank;
     }
-    public void setRank(int i) {
+    private void setRank(int i) {
     	this.rank = i;
     }
     
-    public int getVotes() {
+    private int getVotes() {
     	return this.votes;
     }
-    public void setVotes(int i) {
+    private void setVotes(int i) {
     	this.votes = i;
     }
     
-    public int getCategoryId() {
+    private int getCategoryId() {
     	return this.categoryId;
     }
-    public void setCategoryId(int i) {
+    private void setCategoryId(int i) {
     	this.categoryId = i;
     }
     
-    public int getChampionId() {
+    private int getChampionId() {
     	return this.championId;
     }
-    public void setChampionId(int i) {
+    private void setChampionId(int i) {
     	this.championId = i;
     }
     
-    public String getQa() {
+    private String getQa() {
     	return this.qa;
     }
-    public void setQa(String s) {
+    private void setQa(String s) {
     	this.qa = s;
     }
 
-    public String getComments() {
+    private String getComments() {
     	return this.comments;
     }
-    public void setComments(String s) {
+    private void setComments(String s) {
     	this.comments = s;
     }
 
-    public String getWebUrls() {
+    private String getWebUrls() {
     	return this.webUrls;
     }
-    public void setWebUrls(String s) {
+    private void setWebUrls(String s) {
     	this.webUrls = s;
     }
 }
