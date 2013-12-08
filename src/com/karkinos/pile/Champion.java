b92aac085b89961d15e6213bebb867d8a19fd253
Deric Walintukan <deric.walintukan@gmail.com>
2013-12-08 13:33:43 -0800

    add some varibles to champion class

--- src/com/karkinos/pile/Champion.java
+++ src/com/karkinos/pile/Champion.java
@@ -3,12 +3,46 @@ package com.karkinos.pile;
 // Object class for a champion
 public class Champion {
 	private int id = 0;
-	private String name = "";
 	private int rank = 0;
-	private int finishedDeals = 0;
-	
+	private String name = "";
+	private String location = "";
+	private int championedDeals = 0;
 	
 	public Champion() {
 	}
 	
+	public int getId() {
+		return this.id;
+	}
+	public void setId(int i) {
+		this.id = i;
+	}
+	
+	public int getRank() {
+		return this.rank;
+	}
+	public void setRank(int i) {
+		this.rank = i;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String s) {
+		this.name = s;
+	}
+	
+	public String getLocation() {
+		return this.location;
+	}
+	public void setLocation(String s) {
+		this.location = s;
+	}
+	
+	public int getChampionedDeals() {
+		return this.championedDeals;
+	}
+	public void setChampionedDeals(int i) {
+		this.championedDeals = i;
+	}
 }
