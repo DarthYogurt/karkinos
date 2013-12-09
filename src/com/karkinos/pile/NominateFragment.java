03bdc36b03a726d0a943919f25c9ed420399083e
Deric Walintukan <deric.walintukan@gmail.com>
2013-12-08 17:08:15 -0800

    fixed spinner in fragment

--- src/com/karkinos/pile/NominateFragment.java
+++ src/com/karkinos/pile/NominateFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -22,13 +23,17 @@ public class NominateFragment extends Fragment {
 	    
 	    View view = inflater.inflate(R.layout.fragment_nominate_page, container, false);
 	    
-//	    Spinner spinner = (Spinner)findViewById(R.id.category);
-//		// Create an ArrayAdapter using the string array and a default spinner layout
-//		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_item);
-//		// Specify the layout to use when the list of choices appears
-//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//		// Apply the adapter to the spinner
-//		spinner.setAdapter(adapter);
+	    //SPINNER
+  		Spinner spinner = (Spinner)view.findViewById(R.id.category);
+  		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.category_array, android.R.layout.simple_spinner_item); // Create an ArrayAdapter using the string array and a default spinner layout
+  		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);	// Specify the layout to use when the list of choices appears
+  		spinner.setAdapter(adapter);	// Apply the adapter to the spinner
+  		
+  		//AUTOCOMPLETE
+  		AutoCompleteTextView nominateItem = (AutoCompleteTextView)view.findViewById(R.id.autocomplete_nominate_item); // Get a reference to the AutoCompleteTextView in the layout
+  		String[] suggestedItems = getResources().getStringArray(R.array.suggested_items_array); // Get the string array
+  		ArrayAdapter<String> adapterItem = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, suggestedItems);		// Create the adapter and set it to the AutoCompleteTextView 
+  		nominateItem.setAdapter(adapterItem);
         
         return view;
 	}
